c55576b78f2dae061159815256944d645ebe39ba
lptrk <96191959+lptrk@users.noreply.github.com>
2024-11-29 12:44:09 +0100

    Update pml.rb

--- pml.rb
+++ pml.rb
@@ -2,7 +2,7 @@ class Pml < Formula
   desc "Properties to YAML or YAML to properties converter"
   homepage "https://github.com/lptrk/properties-yamlized"
   url "https://github.com/lptrk/properties-yamlized/releases/download/0.1.0/pml-darwin-arm64", :using => :curl
-  sha256 "d505563d9608e611e3bdda7169dc7e8940170664ac855fb78c04a8c6a065a9"
+  sha256 "9c40613cf0739e82cf7d1840ee8e683c6c8fb012bcddb447d65c6b694e9e637f"
 
   def install
     bin.install "pml-darwin-arm64" => "pml"
