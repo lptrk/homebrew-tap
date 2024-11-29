7e56def5411d81302af996e6d4d9ee66d7f6162c
lptrk <96191959+lptrk@users.noreply.github.com>
2024-11-29 12:43:40 +0100

    Update pml.rb

--- pml.rb
+++ pml.rb
@@ -1,11 +1,11 @@
 class Pml < Formula
   desc "Properties to YAML or YAML to properties converter"
-  homepage "https://github.com/lptrk/properties-yamlized"  # Ersetze 'username' durch deinen GitHub-Benutzernamen
+  homepage "https://github.com/lptrk/properties-yamlized"
   url "https://github.com/lptrk/properties-yamlized/releases/download/0.1.0/pml-darwin-arm64", :using => :curl
-  sha256 "d505563d9608e611e3bdda7169dc7e8940170664ac855fb78c04a8c6a065a9"  
+  sha256 "d505563d9608e611e3bdda7169dc7e8940170664ac855fb78c04a8c6a065a9"
 
   def install
-    bin.install "pml-darwin-amd64" => "pml"
+    bin.install "pml-darwin-arm64" => "pml"
   end
 
   test do
